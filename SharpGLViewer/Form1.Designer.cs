d78ee00465847586bdefb9d77064cf957804b3b9
Douglas Rocha <douglasbellonrocha@gmail.com>
2016-04-12 10:32:39 -0300

    Rendered striped rectangle

--- SharpGLViewer/Form1.Designer.cs
+++ SharpGLViewer/Form1.Designer.cs
@@ -28,12 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openGLControl = new SharpGLViewer.MyOpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.Location = new System.Drawing.Point(0, 0);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(284, 262);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.openGLControl);
+            this.Name = "Form1";
+            this.Text = "SharpGLViewer";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private MyOpenGLControl openGLControl;
+
     }
 }
 
--- SharpGLViewer/Form1.cs
+++ SharpGLViewer/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SharpGL;
+using SharpGL.Version;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,56 @@ namespace SharpGLViewer
 {
     public partial class Form1 : Form
     {
+        private float rotation = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+
+            // Clear the screen and the depth buffer and resets the view
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+
+            gl.Translate(0.0f, 0.0f, -15.0f);
+            gl.Rotate(rotation, 9.0f, 8.0f, 5.0f);
+
+            gl.Begin(OpenGL.GL_POINTS);
+
+            for (var i = -5.0f; i < 5.0f; i += 0.1f)
+            {
+                for (var j = -5.0f; j < 5.0f; j += 0.1f)
+                {
+                    for (var k = -1.0f; k < 1.0f; k += 0.1f)
+                    {
+                        if (i < -3)
+                        {
+                            gl.Color(1.0f, 0.0f, 0.0f);	
+                        }
+                        else if (i > -3 && i < 2)
+                        {
+                            gl.Color(0.0f, 1.0f, 0.0f);	
+                        }
+                        else
+                        {
+                            gl.Color(0.0f, 0.0f, 1.0f);	
+                        }
+
+                        gl.Vertex(i, j, k);
+                    }
+                }
+
+            }
+
+            gl.End();
+            gl.Flush();
+
+            rotation += 3.0f;
+        }
+
     }
 }
